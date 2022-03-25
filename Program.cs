ed093d2ce6bbcf7f0b300aea7ab0652e13688fbc
ilja.tihhonov <ilja.tihhonov@outlook.com>
2022-03-25 09:33:02 +0200

    Grading IF

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace GradingIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your exam result.");
+            char grade = Convert.ToChar(Console.ReadLine().ToUpper());
+            
+            if (grade == 'A')
+            {
+                Console.WriteLine("Outstanding!!!");
+            }
+            else if (grade == 'B')
+            {
+                Console.WriteLine("Superior!!");
+            }
+            else if (grade == 'C')
+            {
+                Console.WriteLine("Good!");
+            }
+            else if (grade == 'D')
+            {
+                Console.WriteLine("Satisfactory.");
+            }
+            else if (grade == 'E')
+            {
+                Console.WriteLine("Low Pass.");
+            }
+            else if (grade == 'F')
+            {
+                Console.WriteLine("Failure.");
+            }
+            else
+            {
+                Console.WriteLine("What?!");
+            }
+        }
+    }
+}
